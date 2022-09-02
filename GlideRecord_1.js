88b609a4c1f5aaaa93d28d43635f103153d696dd
Lulmao <yxchia98@gmail.com>
2022-09-02 16:37:07 +0800

    Added GlideRecord CRUD

---
+++ GlideRecord_1.js
@@ -0,0 +1,72 @@
+// read incidents
+var incidentGR = new GlideRecord("incident");
+incidentGR.query();
+while (incidentGR.next()) {
+  gs.print(incidentGR.number);
+}
+
+// read and query using addQuery, incidents where priority = 1
+var incidentGR = new GlideRecord("incident");
+incidentGR.addQuery("priority", 1);
+incidentGR.query();
+while (incidentGR.next()) {
+  gs.print(
+    "Priority 1 incident: " +
+      incidentGR.number +
+      ":" +
+      incidentGR.priority.getDisplayValue()
+  );
+}
+
+// get api
+// retrieve incident of sys_id 57af7aec73d423002728660c4cf6a71c
+var incidentGR = new GlideRecord("incident");
+incidentGR.get("57af7aec73d423002728660c4cf6a71c");
+gs.print(incidentGR.number + "has a sys_id of:" + incidentGR.sys_id);
+
+// read and query using addEncodedQuery
+var encodedQueryString =
+  "category=inquiry^active=true^opened_by=6816f79cc0a8016401c5a33be04be441";
+var incidentGR = new GlideRecord("incident");
+incidentGR.addEncodedQuery(encodedQueryString);
+incidentGR.query();
+while (incidentGR.next()) {
+  gs.print(
+    "Priority 1 incident: " +
+      incidentGR.number +
+      ":" +
+      incidentGR.priority.getDisplayValue()
+  );
+}
+
+// create new incident
+var newIncident = new GlideRecord("incident");
+newIncident.newRecord();
+newIncident.short_description =
+  "This incident was created from a background script";
+newIncidentSysId = newIncident.insert();
+gs.print(newIncidentSysId);
+
+// create multiple new instances
+var newIncidents = [];
+var counter = 0;
+var incidentGR = new GlideRecord("incident");
+while (counter < 5) {
+  incidentGR.newRecord();
+  incidentGR.short_description = "Incident #" + ++counter;
+  newIncidents.push(incidentGR.insert());
+}
+gs.print(newIncidents);
+newIncidents.map(function (incident) {
+  incidentGR.get(incident);
+  gs.print(incidentGR.number + " has a sys_id of:" + incidentGR.sys_id);
+});
+
+// delete all previously created incident records
+var incidentGR = new GlideRecord("incident");
+incidentGR.addQuery("short_description", "CONTAINS", "Incident #");
+incidentGR.query();
+while (incidentGR.next()) {
+  gs.print(incidentGR.number + " has " + incidentGR.short_description);
+  incidentGR.deleteRecord();
+}
